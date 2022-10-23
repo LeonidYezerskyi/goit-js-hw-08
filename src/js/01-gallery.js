c55d0e0b8811bf2073799d660cb89244135edc90
LeonidYezerskyi <111503058+LeonidYezerskyi@users.noreply.github.com>
2022-10-24 00:25:09 +0300

    Додай бібліотеку SimpleLightbox

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,26 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+const ulEl = document.querySelector('.gallery');
+
+const elements = galleryItems
+  .map(item => {
+      const strHTML = 
+      `<div class="gallery__item">
+       <a class="gallery__item" href="${item.original}">
+        <img class="gallery__image"
+        src="${item.preview}"
+        alt="${item.description}" />
+       </a>
+      </div>`;
+    return strHTML;
+  })
+  .join("");
+ulEl.insertAdjacentHTML("afterbegin", elements);
+
+new SimpleLightbox('.gallery a', { captionsData: "alt",  captionDelay: 250, });
 
 console.log(galleryItems);
