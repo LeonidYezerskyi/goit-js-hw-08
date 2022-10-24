16dd1335f8f431bdc0c3042d15aa3b16065b78d6
LeonidYezerskyi <111503058+LeonidYezerskyi@users.noreply.github.com>
2022-10-24 16:18:36 +0300

    Виконує завдання 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,19 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
+const key = 'videoplayer-current-time';
+const vimeo = document.querySelector('#vimeo-player');
+const player = new Player(vimeo);
+const currentTime = localStorage.getItem(key);
+
+if (currentTime) {
+    player.setCurrentTime(currentTime);
+};
+
+player.on('timeupdate', throttle(setCurrentTimeInStorage, 1000));
+
+function setCurrentTimeInStorage(data) {
+    localStorage.setItem(key, data.seconds);
+    console.log(data.seconds.toFixed(1));
+};
 
