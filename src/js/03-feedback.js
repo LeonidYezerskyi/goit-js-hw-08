b653222020e41aacf7fb2f3d7e9648a2d740d906
LeonidYezerskyi <111503058+LeonidYezerskyi@users.noreply.github.com>
2022-10-24 23:13:57 +0300

    Виконує завдання 3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,38 @@
+import throttle from 'lodash.throttle';
 
+const STORAGE_KEY = 'feedback-form-state';
+
+const form = document.querySelector('.feedback-form');
+const email = document.querySelector('.feedback-form input');
+const textarea = document.querySelector('.feedback-form textarea');
+
+form.addEventListener('submit', onFormSubmit);
+form.addEventListener('input', throttle(onInput, 500));
+
+fillInput();
+
+function onInput(event) {
+    const savedFormData = localStorage.getItem(STORAGE_KEY);
+    const formData = JSON.parse(savedFormData) || {};
+    formData[event.target.name] = event.target.value;
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+}
+
+function onFormSubmit(event) {
+    event.preventDefault();
+    console.log({
+        email: event.currentTarget.elements.email.value,
+        message: event.currentTarget.elements.message.value,
+    });
+    event.currentTarget.reset();
+    localStorage.removeItem(STORAGE_KEY);
+}
+
+function fillInput() {
+    const savedFormData = localStorage.getItem(STORAGE_KEY);
+    const parsedFormData = JSON.parse(savedFormData);
+    if (parsedFormData) {
+        email.value = parsedFormData.email || '';
+        textarea.value = parsedFormData.message || '';
+    }
+}
